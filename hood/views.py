59fa5262bff15a40bc3478df95ec81fe30f40296
pascaline <passyish5@gmail.com>
2019-10-30 14:18:53 +0200

    added datetime

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt 
 
-# Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my neighborhood')
+    date = dt.date.today()
+    return render(request, 'welcome.html', {"date": date})
