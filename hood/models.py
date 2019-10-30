02b1771a83e55b351e353454c51d9bb387db8195
pascaline <passyish5@gmail.com>
2019-10-30 15:27:56 +0200

    installed django registration

--- hood/models.py
+++ hood/models.py
@@ -4,3 +4,7 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length =30)
     location = models.CharField(max_length =30)
     occupants = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+        
--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt 
+from .models import *
 
 def welcome(request):
     date = dt.date.today()
-    return render(request, 'welcome.html', {"date": date})
+    new = NeighbourHood.objects.all()
+    return render(request, 'welcome.html', {"date": date, "new": new})
