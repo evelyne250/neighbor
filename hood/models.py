9c90d8c10539afad16b30330ca5d76d8d352ece1
pascaline <passyish5@gmail.com>
2019-10-30 14:56:04 +0200

    created first model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length =30)
+    location = models.CharField(max_length =30)
+    occupants = models.IntegerField()
