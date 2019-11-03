32a613c5afe0e9fc70b9718a4193fd1ea75c25be
pascaline <passyish5@gmail.com>
2019-11-03 18:13:16 +0200

    added tests

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.eve = Profile( user = 'eve', profile_picture  = '/', bio = 'my tests', contact='uevelyne44@gmail.com')
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.eve,Profile))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.eve.save_profile()
+        eve = Profile.objects.all()
+        self.assertTrue(len(eve) > 0)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+       
